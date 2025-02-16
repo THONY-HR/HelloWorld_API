0ab24f9f1043b14b8ad91b4d66a0a2f7479897d4
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-16 12:29:15 +0300

    Ajout index

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,11 @@ app.use(express.json()); // Pour parser le JSON
 
 // Utilise les routes pour les utilisateurs
 app.use('/user', userRoutes);
+
+// Route pour afficher la page HTML à la racine
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
