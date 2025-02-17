09b45dc190c120ae6eb2d5046914195a3a200c28
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-17 20:07:49 +0300

    Modification base donner , enlever mot de passe

--- prisma/migrations/20250216101617_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `videoUrl` on the `Utilisateur` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Utilisateur` DROP COLUMN `videoUrl`;
--- prisma/migrations/20250217154417_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `motDePasse` on the `Utilisateur` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Utilisateur` DROP COLUMN `motDePasse`;
