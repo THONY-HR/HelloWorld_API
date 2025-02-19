1a09bf855f223700129b58434da7539df933b30c
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-19 10:36:33 +0300

    Reglage cors

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,29 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
 import utilisateurRoutes from './routes/utilisateurRoutes';
 import communauteRoutes from './routes/communauteRoutes';
-
-import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Pour parser le JSON
+// Configuration de CORS pour autoriser toutes les origines
+app.use(cors()); // Accepte toutes les URL
 
+// Middleware pour parser le JSON
+app.use(express.json());
 
-// Utilise les routes pour les utilisateurs
+// Utilise les routes pour les utilisateurs et les communautés
 app.use('/utilisateur', utilisateurRoutes);
 app.use('/communaute', communauteRoutes);
-
 
 // Route pour afficher la page HTML à la racine
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+// Lancement du serveur
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
