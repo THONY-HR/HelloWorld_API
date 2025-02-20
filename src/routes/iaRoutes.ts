5f7374f102fec44cba87c8768f400fa956168158
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-20 20:31:41 +0300

    Ajout deepseek-free

--- src/routes/iaRoutes.ts
+++ src/routes/iaRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { chatMistral, chatDeepseek8B,chatDeepseek32B,chatGPT4Turbo} from '../controllers/iaController';
+import { chatMistral, chatDeepseek8B,chatDeepseek32B,chatQwen,chatDeepseekFree} from '../controllers/iaController';
 
 const router = Router();
 
-router.post('/mistral', chatMistral);
+router.post('/mistral-24B', chatMistral);
+router.post('/deepseek-free', chatDeepseekFree);
 router.post('/deepseek-8B', chatDeepseek8B);
 router.post('/deepseek-32B', chatDeepseek32B);
-router.post('/gpt4-Turbo', chatGPT4Turbo);
+router.post('/qwen-turbo', chatQwen);
 
 export default router;
